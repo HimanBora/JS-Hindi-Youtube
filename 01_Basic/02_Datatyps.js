1f6ea83209a27d6653be570f55b2e754ba89e8d7
HimanBora <152855199+HimanBora@users.noreply.github.com>
2024-08-06 04:52:39 +0000

    Datatypes

---
+++ 01_Basic/02_Datatyps.js
@@ -0,0 +1,24 @@
+"use strict" ; //treat all JS code as newers version of JS standard
+
+//alert(3+3) //we are using nodejs, not browser so the code won't run
+
+console.log(3+3); console.log('Himan Bora')
+// the above code is not good as it has no readability
+// the main goal while writing should be that code readibility should be high
+
+let name = "himan"
+let age = 18
+let isLoggedIn =  true ;
+
+// number => 2 to power 53 (it is the range)
+// bigint => number but no limit
+//string => pattern of text written under ""
+//boolean => true or false
+//null => stand alone value
+//undefined => you have not declaired or set a value
+//symbol =>  uniqueness
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
