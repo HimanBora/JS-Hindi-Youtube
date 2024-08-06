24e620abe86f4bf09552295d73562de091101842
HimanBora <152855199+HimanBora@users.noreply.github.com>
2024-08-06 04:52:21 +0000

    Variable

---
+++ 01_Basic/01_Variable.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "himanbora@gmail.com"
+var accountPassword = "12345"
+accountCity = "Nagaon"
+let accountPlace;
+
+accountEmail = "boraHiman@gmail.com"
+accountPassword = "34567"
+accountCity = "Guwahati"
+
+console.table([accountEmail, accountPassword, accountCity, accountPlace])
