63e56823b1ec73f41f9af62cfa991adc8a53bd4b
HimanBora <152855199+HimanBora@users.noreply.github.com>
2024-08-07 07:37:05 +0000

    Comparision and conversion

--- 01_Basic/03_Conversion.js
+++ 01_Basic/03_Conversion.js
@@ -22,3 +22,42 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33;
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
+
+
+//****************Operations******************
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2*2);
+console.log(2**2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Himan"
+let str3 = str1 + str2 ;
+console.log(str3)
+
+console.log(2 + 1 + "2");
+//because 2 int were first so they got added and then converted into string
+
+console.log("1" + 2 + 2);
+//because string is first the entire stuf was converted into string
+
+console.log(3 + 4 * 5 % 3); //Bad way of writing
+
+console.log((3 + 4) * 5 % 3); // right way
+
+console.log(+true) ; // Bad way >2
+console.log(+"") ; // Bad way  > 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++ ;
+console.log(gameCounter);
---
+++ 01_Basic/04_Comparison.js
@@ -0,0 +1,19 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2<=1);
+
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//Comparison ">,<,<=,>=" and "==" are different cause comparison converts the value to number but equallity don't.
+
+// === this is called strict equal as it check the datatypes also.
+
+console.log("2" === 2); // It will result to false
