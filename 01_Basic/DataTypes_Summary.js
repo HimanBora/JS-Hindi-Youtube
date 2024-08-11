069f48cbd9e3bcc98fce1bb95670e20feab68c32
HimanBora <152855199+HimanBora@users.noreply.github.com>
2024-08-11 07:58:54 +0000

    datatypes

---
+++ 01_Basic/DataTypes_Summary.js
@@ -0,0 +1,25 @@
+// Primitive
+
+    // 7 types : String, Number, Boolean, null, Undefined, Symbol, BigInt.
+
+    const score = 100 ;  //Number
+    const scoreValue = 10.3;  //Number
+    const isLoggedIn = false; //Booleans
+    const outsideTemp = null ; //null
+    let userEmail; //Undefined
+    const id = Symbol(123); //Symbol
+    const bigNumber = 123n ; //BigInt 
+    
+//Refference Type(Non Primitive)
+
+    //Array, Objects, Functions.
+    
+    const heroes = ['Spiderman','Iron Man','Doga'] ; //Array
+    let Obj = {
+        // Object
+        name : 'Himan',
+        age : '20'
+    };
+
+    const myFunction = function(){}; //Function
+
