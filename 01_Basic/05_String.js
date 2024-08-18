0cd33ed1a53a3d859cc734e7a0ed76298f66084f
HimanBora <152855199+HimanBora@users.noreply.github.com>
2024-08-18 04:07:51 +0000

    String

--- 01_Basic/05_String.js
+++ 01_Basic/05_String.js
@@ -9,5 +9,19 @@ console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt('2'));
 console.log(gameName.indexOf('i'));
-const newString = gameName.substring(0, 4)// output: H, i, m
+const newString = gameName.substring(0, 4)// output: H, i, m, a, n
 const againString = gameName.slice(-8, 4) //output : i, m, a
+
+const newString = gameName.substring(-8, 4)// output: Himan | reason: substring ignores negative values and starts from 0 if you provide one.
+
+
+const newStringOne = '  himan  '
+console.log(newStringOne)//output: '  himan  '
+
+console.log(newStringOne.trim)//output: 'himan' | Does : remove unwanted spaces
+
+const url = 'https://himan.com/himan%20bora'
+
+url.replace('%20','_') //output: 'https://himan.com/himan_bora' | Does: replaces a particular substring within a string
+
+url.includes('himan') //output: True | Does: find out is this string is a part of the other string. 
